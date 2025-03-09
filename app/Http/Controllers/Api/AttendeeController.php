6cdd2091abf4c12bc82611b62d00cf75f0d11da4
VishwanathOnGit <putchavishwanath@gmail.com>
2025-03-09 19:35:44 +0530

    Refactor AttendeeController to utilize CanLoadRelationships trait for dynamic relationship loading

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResourse;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
-
-        return AttendeeResourse::collection(
-            $attendees->paginate()
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
         );
     }
 
@@ -27,9 +30,11 @@ class AttendeeController extends Controller
      */
     public function store(Request $request, Event $event)
     {
-        $attendee = $event->attendees()->create([
-            'user_id' => 1
-        ]);
+       $attendee = $this->loadRelationships(
+             $event->attendees()->create([
+                 'user_id' => 1
+             ])
+         );
 
         return new AttendeeResourse($attendee);
     }
@@ -39,7 +44,9 @@ class AttendeeController extends Controller
      */
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResourse($attendee);
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     /**
